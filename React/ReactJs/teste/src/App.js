98d8af94ec89afdb82737958ba27ff13b96e702d
Davi Dias <davi.dias@gmail.com>
2021-06-03 21:50:58 -0300

    03/06/21-21:50

--- React/ReactJs/teste/src/App.js
+++ React/ReactJs/teste/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './Header'
+import Counter from './Counter'
 import './App.css';
 
 function App() {
-  return (
+  return (<div>
    <Header name="Teste" links={["Sobre", "Comprar", "Contato", "Login", "Teste"]}></Header>
-  );
+  <Counter count={5}></Counter>
+  </div>
+   );
 }
 
 export default App;
